5212be4d737208ddedaeb5d83b0ff1e64b13f40f
KangFeeder090403 <alvitoudayalfariz@student.telkomuniversity.ac.id>
2024-05-11 17:05:46 +0700

    update

--- server-side.py
+++ server-side.py
@@ -1,90 +1,137 @@
-import os
 import socket
+import sys
+import time
+import os
+import struct
 
-def file_size(filename):
-    return os.path.getsize(filename) / (1024 * 1024)
+print("\nMenunggu Koneksi dari Client\n")
 
-def run_server(host='localhost', port=12345):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(1)
-    print(f"Server running on {host}:{port}")
-    
-    try:
-        while True:
-            conn, addr = server_socket.accept()
-            print(f"Connected by {addr}")
+TCP_IP = "127.0.0.1"
+TCP_PORT = 1456
+BUFFER_SIZE = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+conn, addr = s.accept()
 
-            try:
-                while True:
-                    data = conn.recv(1024).decode()
-                    if not data:
-                        break
-                    print(f"Received command: {data}")
+print("\n Connection with address : {}".format(addr))
 
-                    command = data.split()[0]
-                    response = ''
+def upld():
+    conn.send(b"1")
+    file_name_length = struct.unpack("h", conn.recv(2))[0]
+    file_name = conn.recv(file_name_length).decode()
 
-                    if command == 'ls':
-                        files = os.listdir('.')
-                        response = '\n'.join(files)
-                    elif command == 'rm':
-                        filename = data.split()[1]
-                        try:
-                            os.remove(filename)
-                            response = f"File {filename} deleted."
-                        except FileNotFoundError:
-                            response = "File not found."
-                    elif command.startswith('download'):
-                        _, filename = data.split()
-                        try:
-                            if not os.path.isfile(filename):
-                                conn.sendall(b'ERROR: File not found.')
-                            else:
-                                conn.sendall(b'OK')
-                                with open(filename, 'rb') as f:
-                                    chunk = f.read(1024)
-                                    while chunk:
-                                        conn.sendall(chunk)
-                                        chunk = f.read(1024)
-                                conn.sendall(b'ENDOFFILE')
-                                print(f"File {filename} has been sent.")
-                        except Exception as e:
-                            response = f"ERROR: {str(e)}"
-                            conn.sendall(response.encode())
-                    elif command.startswith('upload'):
-                        _, filename, new_filename = data.split()
-                        response = 'OK'.encode()
-                        conn.sendall(response)
-                        with open(new_filename, 'wb') as f:
-                            while True:
-                                data = conn.recv(1024)
-                                if not data:
-                                    break
-                                f.write(data)
-                        print(f"File uploaded as {new_filename}.")
-                    elif command == 'size':
-                        filename = data.split()[1]
-                        try:
-                            response = f"Size of {filename}: {file_size(filename):.2f} MB"
-                        except FileNotFoundError:
-                            response = "File not found."
-                    elif command == 'byebye':
-                        conn.close()
-                        print("Connection closed.")
-                        break
-                    elif command == 'connme':
-                        response = "Connection established."
-                    else:
-                        response = "Invalid command."
-                    
-                    conn.sendall(response.encode())
+    original_file_name = file_name
+    counter = 1
+    while os.path.exists(file_name):
+        file_name = f"{os.path.splitext(original_file_name)[0]}_{counter}{os.path.splitext(original_file_name)[1]}"
+        counter += 1
 
-            finally:
-                conn.close()
+    conn.send(b"1")
+    file_size = struct.unpack("i", conn.recv(4))[0]
+    start_time = time.time()
+    print(f"Receiving file: {file_name}")
+    content = open(file_name, "wb")
+    l = conn.recv(BUFFER_SIZE)
+    while l:
+        content.write(l)
+        l = conn.recv(BUFFER_SIZE)
+    content.close()
+    conn.send(struct.pack("f", time.time() - start_time))
+    conn.send(struct.pack("i", file_size))
+    print("File received successfully")
+    return
 
-    finally:
-        server_socket.close()
+def list_files():
+    print("Listing files...")
+    listing = os.listdir(os.getcwd())
+    conn.send(struct.pack("i", len(listing)))
+    total_directory_size = 0
+    for i in listing:
+        conn.send(struct.pack("i", sys.getsizeof(i)))
+        conn.send(i.encode())
+        conn.send(struct.pack("i", os.path.getsize(i)))
+        total_directory_size += os.path.getsize(i)
+        conn.recv(BUFFER_SIZE)
+    conn.send(struct.pack("i", total_directory_size))
+    conn.recv(BUFFER_SIZE)
+    print("Successfully sent file listing")
+    return
 
-if __name__ == "__main__":
-    run_server()
+def dwld():
+    conn.send(b"1")
+    file_name_length = struct.unpack("h", conn.recv(2))[0]
+    file_name = conn.recv(file_name_length).decode()
+    if os.path.isfile(file_name):
+        conn.send(struct.pack("i", os.path.getsize(file_name)))
+    else:
+        print("File name not valid")
+        conn.send(struct.pack("i", -1))
+        return
+    conn.recv(BUFFER_SIZE)
+    start_time = time.time()
+    print("Sending file...")
+    content = open(file_name, "rb")
+    l = content.read(BUFFER_SIZE)
+    while l:
+        conn.send(l)
+        l = content.read(BUFFER_SIZE)
+    content.close()
+    conn.recv(BUFFER_SIZE)
+    conn.send(struct.pack("f", time.time() - start_time))
+    print("File sent successfully")
+    return
+
+def delf():
+    conn.send(b"1")
+    file_name_length = struct.unpack("h", conn.recv(2))[0]
+    file_name = conn.recv(file_name_length).decode()
+    if os.path.isfile(file_name):
+        conn.send(struct.pack("i", 1))
+    else:
+        conn.send(struct.pack("i", -1))
+    confirm_delete = conn.recv(BUFFER_SIZE).decode()
+    if confirm_delete == "Y":
+        try:
+            os.remove(file_name)
+            conn.send(struct.pack("i", 1))
+        except:
+            print("Failed to delete {}".format(file_name))
+            conn.send(struct.pack("i", -1))
+    else:
+        print("Delete abandoned by client!")
+        return
+
+def get_file_size():
+    conn.send(b"1")
+    file_name_length = struct.unpack("h", conn.recv(2))[0]
+    file_name = conn.recv(file_name_length).decode()
+    if os.path.isfile(file_name):
+        conn.send(struct.pack("i", os.path.getsize(file_name)))
+    else:
+        conn.send(struct.pack("i", -1))
+    return
+
+def quit():
+    conn.send(b"1")
+    conn.close()
+    s.close()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+while True:
+    print("\n\nWaiting for instruction")
+    data = conn.recv(BUFFER_SIZE).decode()
+    print("\nReceived instruction: {}".format(data))
+    if data == "upload":
+        upld()
+    elif data == "ls":
+        list_files()
+    elif data == "download":
+        dwld()
+    elif data == "rm":
+        delf()
+    elif data == "size":
+        get_file_size()
+    elif data == "byebye":
+        quit()
+    data = None
